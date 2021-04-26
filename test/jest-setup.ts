e7657deb395bc5c32d073619a9a9288683424924
IglanCardeal <cmtcardeal@outlook.com>
2021-04-26 11:03:46 -0300

    fix: fixing some issue related to eslint

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,11 +1,8 @@
-/**
- * Este arquivo é responsável por inicializar o servidor para todos os
- * testes funcionais.
- */
-
 import { SetupServer } from '@src/server'
 import supertest from 'supertest'
 
+// Este arquivo é responsável por inicializar o servidor para todos os
+// testes funcionais.
 beforeAll(() => {
   const server = new SetupServer()
 
@@ -13,5 +10,7 @@ beforeAll(() => {
 
   const app = server.getApp()
 
+  // criamos "testRequest" para o objeto
+  // global com os métodos do supertest 
   global.testRequest = supertest(app)
 })
