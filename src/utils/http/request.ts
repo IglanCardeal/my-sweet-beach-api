9dec03fb5af2da5758249c23f5673972accece13
IglanCardeal <cmtcardeal@outlook.com>
2021-08-11 15:42:12 -0300

    feat: creating get method do Request class

--- src/utils/http/request.ts
+++ src/utils/http/request.ts
@@ -1,7 +1,34 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
 
+interface RequestConfig extends AxiosRequestConfig {
+  [key: string]: any
+}
+
+interface Response<T = any> extends AxiosResponse<T> {
+  [key: string]: any
+}
+
+/**
+ *  @classdesc Classe para realizar chamadas HTTP usando libs externas.
+ *  @constructor {Object} `requester` recebe o módulo que fará as chamadas
+ *  externas para a API. Default: `axios`.
+ */
 export class Request {
   constructor (private requester = axios) {
     this.requester = requester
+  }
+
+  public async get<T> (
+    url: string,
+    requestConfig: RequestConfig = {}
+  ): Promise<Response<T>> {
+    console.log(url, requestConfig)
+
+    const response = await this.requester.get<T, Response<T>>(
+      url,
+      requestConfig
+    )
+
+    return response
   }
 }
