329c11ab510e614a12dadfd68c102e665057bb6c
IglanCardeal <cmtcardeal@outlook.com>
2021-08-16 10:42:46 -0300

    refactor: creating a function to merge beach and point datas

--- src/services/forecast-service.ts
+++ src/services/forecast-service.ts
@@ -43,20 +43,29 @@ export class ForecastService {
     for (const beach of beaches) {
       const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng)
       // normaliza os dados fazendo um merge
-      const enrichedBeachData = points.map(e => ({
-        ...{
-          lat: beach.lat,
-          lng: beach.lng,
-          name: beach.name,
-          position: beach.position,
-          rating: 1
-        },
-        ...e
-      }))
+      const enrichedBeachData = points.map(e =>
+        this.mergeBeachAndPointData(e, beach)
+      )
 
       pointsWithCorrectedSources.push(...enrichedBeachData)
     }
 
     return pointsWithCorrectedSources
+  }
+
+  private mergeBeachAndPointData (
+    point: StormGlassForecastAPIResponseNormalized,
+    beach: Beach
+  ): BeachForecast {
+    return {
+      ...{
+        lat: beach.lat,
+        lng: beach.lng,
+        name: beach.name,
+        position: beach.position,
+        rating: 1
+      },
+      ...point
+    }
   }
 }
