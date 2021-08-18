62ccc5f9c26bcd0401800e594c8fed8dc5a5f80c
IglanCardeal <cmtcardeal@outlook.com>
2021-08-18 00:38:15 -0300

    feat: creating and adding a beaches controller class

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import './utils/module-alias'
 import { Server } from '@overnightjs/core'
 import { Application, json } from 'express'
 import { ForecastController } from './controllers/forecast-controller'
+import { BeachesController } from './controllers/beaches-controller'
 import { Database } from './database'
 
 export class SetupServer extends Server {
@@ -35,8 +36,9 @@ export class SetupServer extends Server {
 
   private setupControllers (): void {
     const forecastController = new ForecastController()
+    const beachesController = new BeachesController()
 
-    this.addControllers([forecastController])
+    this.addControllers([forecastController, beachesController])
   }
 
   private async setupDatabase (): Promise<void> {
