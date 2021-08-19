d037359bfbd0f40168d277cfe9aea933710bf1a0
IglanCardeal <cmtcardeal@outlook.com>
2021-08-18 23:55:48 -0300

    feat: creating beach model

---
+++ src/models/beaches-model.ts
@@ -0,0 +1,42 @@
+/* eslint-disable no-unused-vars */
+import { Schema, Document, model } from 'mongoose'
+
+enum BeachPosition {
+  S = 'S',
+  E = 'E',
+  W = 'W',
+  N = 'N'
+}
+
+interface Beach {
+  _id?: string
+  lat: number
+  lng: number
+  name: string
+  position: BeachPosition
+}
+
+interface BeachDocument extends Omit<Beach, '_id'>, Document {}
+
+const schema = new Schema(
+  {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+    name: { type: String, required: true },
+    position: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    toJSON: {
+      transform: (_, doc): void => {
+        doc.id = doc._id
+        delete doc._id
+        delete doc.__v
+      }
+    }
+  }
+)
+
+export const BeachModel = model<BeachDocument>('Beach', schema)
