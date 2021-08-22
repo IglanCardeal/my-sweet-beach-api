a3d6bff3e8c5f75f00c469fff810f1c7f71c55e9
IglanCardeal <cmtcardeal@outlook.com>
2021-08-22 18:32:34 -0300

    test: adding a test case when the user email already exist in the database

--- test/functional/users.spec.ts
+++ test/functional/users.spec.ts
@@ -21,16 +21,32 @@ describe('Users functional test', () => {
 
     it('should return 422 when there is validation error', async () => {
       const newUser = {
-        email: 'foo@mail.com',
+        email: 'foo2@mail.com',
         password: '123456'
       }
 
       const response = await global.testRequest.post('/users').send({ newUser })
 
       expect(response.status).toBe(422)
-      expect(response.body).toEqual({ 
+      expect(response.body).toEqual({
         code: 422,
-        error: 'Users validation failed: name: Path `name` is required.'
+        error: 'User validation failed: name: Path `name` is required.'
+      })
+    })
+
+    it('should return 409 when the email is already in use', async () => {
+      const newUser = {
+        name: 'Foo Bar',
+        email: 'foo@mail.com',
+        password: '123456'
+      }
+
+      const response = await global.testRequest.post('/users').send({ newUser })
+
+      expect(response.status).toBe(409)
+      expect(response.body).toEqual({
+        code: 409,
+        error: 'User validation failed: email: already exist in the database.'
       })
     })
   })
