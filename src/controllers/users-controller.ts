65efb278f352fa0c89850df14a039d32e229bb3c
IglanCardeal <cmtcardeal@outlook.com>
2021-08-22 15:17:55 -0300

    feat: using user model on users controller

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,5 +1,8 @@
 import { Controller, Post } from '@overnightjs/core'
 import { Request, Response } from 'express'
+import { Error, mongo } from 'mongoose'
+
+import { UserModel } from '@src/models/user-model'
 
 @Controller('users')
 export class UsersController {
@@ -9,11 +12,16 @@ export class UsersController {
     res: Response
   ): Promise<void> {
     try {
-      const { newUser } = req.body
+      const user = new UserModel(req.body.newUser)
+      const response = await user.save()
 
-      res.status(201).send(newUser)
+      res.status(201).send(response)
     } catch (error) {
-      console.error(error)
+      if (error instanceof Error.ValidationError)
+        res.status(400).send({ error: error.message })
+
+      if (error instanceof mongo.MongoError)
+        res.status(401).send({ error: error.message })
 
       if (error instanceof Error)
         res.status(500).send({ error: 'Internal Server Error' })
