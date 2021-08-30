81dd1798e4dfae35c2a101359c74f6f5a19cc969
IglanCardeal <cmtcardeal@outlook.com>
2021-08-30 18:31:10 -0300

    test: adding a jwt token malformed test case

--- src/middlewares/__test__/auth-middle.test.ts
+++ src/middlewares/__test__/auth-middle.test.ts
@@ -18,4 +18,25 @@ describe('Auth Middleware', () => {
     expect(nextFake).toHaveBeenCalled()
     expect(reqFake.decoded).toBeDefined()
   })
+
+  it('should return UNAUTHORIZED if there is a problem on the token verification', () => {
+    const reqFake = {
+      headers: {
+        'x-access-token': 'invalid-jwtToken'
+      },
+      decoded: undefined
+    }
+    const sendMock = jest.fn()
+    const resFake = {
+      status: jest.fn(() => ({
+        send: sendMock
+      }))
+    }
+    const nextFake = jest.fn()
+
+    authMiddleware(reqFake, resFake as any, nextFake)
+
+    expect(resFake.status).toHaveBeenCalledWith(401)
+    expect(sendMock).toHaveBeenCalledWith({ code: 401, error: 'jwt malformed' })
+  })
 })
