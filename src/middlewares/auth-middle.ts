7f32a4fce95d6418294c031c93d29e47d0b1a7bb
IglanCardeal <cmtcardeal@outlook.com>
2021-08-30 20:29:38 -0300

    refactor: adding the user decoded data to request object

--- src/middlewares/auth-middle.ts
+++ src/middlewares/auth-middle.ts
@@ -3,13 +3,18 @@ import { NextFunction, Request, Response } from 'express'
 
 export const authMiddleware = (
   req: Partial<Request>,
-  _: Partial<Response>,
+  res: Partial<Response>,
   next: NextFunction
 ): void => {
-  const token = req.headers?.['x-access-token']
-  const decodedUser = AuthService.decodeToken(token as string)
+  try {
+    const token = req.headers?.['x-access-token']
+    const decodedUserData = AuthService.decodeToken(token as string)
 
-  req.decoded = decodedUser
+    req.decoded = decodedUserData
 
-  next()
+    next()
+  } catch (error) {
+    if (error instanceof Error)
+      res?.status?.(401).send({ code: 401, error: error.message })
+  }
 }
