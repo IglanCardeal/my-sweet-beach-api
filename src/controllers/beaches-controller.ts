fa563d83415e42855705e338f5aa84964573cdcd
IglanCardeal <cmtcardeal@outlook.com>
2021-08-30 20:27:08 -0300

    feat: adding middleware to restrict controller to only auth users

--- src/controllers/beaches-controller.ts
+++ src/controllers/beaches-controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Post } from '@overnightjs/core'
+import { ClassMiddleware, Controller, Post } from '@overnightjs/core'
 import { Request, Response } from 'express'
 import { Error } from 'mongoose'
 
 import { BeachModel } from '@src/models/beach-model'
+import { authMiddleware } from '@src/middlewares/auth-middle'
 
 @Controller('beaches')
+@ClassMiddleware(authMiddleware)
 export class BeachesController {
   @Post('')
   public async create (req: Request, res: Response): Promise<void> {
     const { newBeach } = req.body
 
     try {
-      const beach = new BeachModel(newBeach)
+      const beach = new BeachModel({ ...newBeach, user: req.decoded?.id })
       const result = await beach.save()
 
       res.status(201).send(result)
