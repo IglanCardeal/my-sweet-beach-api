ac4f9cd5116c5e85a4479ccaf462ebac4c785b4b
IglanCardeal <cmtcardeal@outlook.com>
2021-08-30 21:46:04 -0300

    fix: adding random user id to forecast unit test

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -23,7 +23,7 @@ describe('Forecast Service', () => {
         lng: 151.289824,
         name: 'Manly',
         position: BeachPosition.E,
-        // user: 'some-id'
+        user: 'some-id'
       }
     ]
     const forecast = new ForecastService(mockedStormGlassHttpClient)
@@ -46,7 +46,7 @@ describe('Forecast Service', () => {
         lng: 151.289824,
         name: 'Manly',
         position: BeachPosition.E,
-        // user: 'some-id'
+        user: 'some-id'
       }
     ]
 
