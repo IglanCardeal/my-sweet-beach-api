e494f8f0d5103566aab6b8113937e1ff956fa9a4
Iglan Cardeal <cmtcardeal@outlook.com>
2021-09-03 12:07:15 -0300

    test: creating pre condition and beach creation test cases

--- src/services/beach/__test__/create-beach-service.test.ts
+++ src/services/beach/__test__/create-beach-service.test.ts
@@ -1,14 +1,41 @@
+import { BeachDTO } from '../beach-dto'
 import { CreateBeachService } from '../create-beach-service'
+import { beaches, InMemoryRepo } from './in-memory-repository'
 
 describe('Create Beach Service', () => {
-  it('should have a "execute" method defined', () => {
-    const fakeRepositoy = {
-      create: async () => {
-        return
-      }
-    }
-    const beachService = new CreateBeachService(fakeRepositoy)
+  beforeAll(() => {
+    beaches.length = 0
+  })
 
-    expect(beachService.execute).toBeDefined()
+  describe('Pre Conditions', () => {
+    it('should have an "execute" method defined', () => {
+      const fakeRepositoy = {
+        create: async () => {
+          return
+        }
+      }
+      const beachService = new CreateBeachService(fakeRepositoy)
+
+      expect(beachService.execute).toBeDefined()
+    })
+
+    it('should "beachs" in memory repository to be empty', () => {
+      expect(beaches).toHaveLength(0)
+    })
+  })
+
+  it('should create a beach successfully', async () => {
+    const beach: BeachDTO = {
+      user: 'fake-id',
+      lat: 1,
+      lng: 1,
+      name: 'fake baech',
+      position: 'E'
+    }
+    const beachService = new CreateBeachService(new InMemoryRepo())
+
+    await beachService.execute(beach)
+
+    expect(beaches).toHaveLength(1)
   })
 })
--- src/services/beach/__test__/in-memory-repository.ts
+++ src/services/beach/__test__/in-memory-repository.ts
@@ -1,7 +1,7 @@
 import { BeachDTO } from '../beach-dto'
 import { BeachRepo } from '../ports/beach-repo'
 
-const beaches: BeachDTO[] = []
+export const beaches: BeachDTO[] = []
 
 export class InMemoryRepo implements BeachRepo {
   public async create (data: BeachDTO): Promise<void> {
