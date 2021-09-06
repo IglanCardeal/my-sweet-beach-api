afbd12547fb31100149bb0b1e91db3e6484bd081
Iglan Cardeal <cmtcardeal@outlook.com>
2021-09-06 11:16:11 -0300

    test: implementing in memory repo and fixing user creation test

--- src/services/user/__test__/create-user-service.test.ts
+++ src/services/user/__test__/create-user-service.test.ts
@@ -12,11 +12,10 @@ describe('Create User Service', () => {
     password: '123456'
   }
   const inUserMemoRepo = new InMemoryUserRepo()
-  console.log(inUserMemoRepo)
 
   describe('Pre Conditions', () => {
     it('should have an "execute" method', () => {
-      const createUserService = new CreateUserService()
+      const createUserService = new CreateUserService(inUserMemoRepo)
 
       expect(createUserService.execute).toBeDefined()
     })
@@ -27,10 +26,10 @@ describe('Create User Service', () => {
   })
 
   it('should create a new user and insert it on users', async () => {
-    const createUserService = new CreateUserService()
+    const createUserService = new CreateUserService(inUserMemoRepo)
 
     await expect(createUserService.execute(userData)).resolves.toBeUndefined()
-    expect(users.length).toHaveLength(1)
+    expect(users).toHaveLength(1)
     expect(users[0].email).toBe(userData.email)
   })
 })
--- src/services/user/__test__/in-memory-repository.ts
+++ src/services/user/__test__/in-memory-repository.ts
@@ -1,10 +1,17 @@
-import { UserRepo } from '../auth-user-service'
+import { UserRepo as UserAuthRepo } from '../auth-user-service'
+import { UserRepo as UserCreationRepo } from '../create-user-service'
 import { UserDTO } from '../user-dto'
 
 export const users: UserDTO[] = []
 
-export class InMemoryUserRepo implements UserRepo {
-  public async findUserByEmail(email: string): Promise<UserDTO> {
-    return users.filter((user) => user.email === email)[0]
+interface InMemoryRepo extends UserAuthRepo, UserCreationRepo {}
+
+export class InMemoryUserRepo implements InMemoryRepo {
+  public async findUserByEmail (email: string): Promise<UserDTO> {
+    return users.filter(user => user.email === email)[0]
+  }
+
+  public async createUser (userData: UserDTO): Promise<void> {
+    users.push(userData)
   }
 }
