b5e8f66826b9f8e6d8d5b58ccfb270c95e748eaf
Iglan Cardeal <cmtcardeal@outlook.com>
2021-09-08 11:26:05 -0300

    feat: adding graceful shutdown

--- src/infra/index.ts
+++ src/infra/index.ts
@@ -1,14 +1,53 @@
-import { SetupServer } from './server'
 import config from 'config'
 
-;(async () => {
-  const PORT = config.get('App.port') as number
+import { SetupServer } from './server'
+import { Logger } from './logger'
 
-  const server = new SetupServer(PORT)
+process.on('unhandledRejection', (reason, promise) => {
+  Logger.error(
+    `App exiting due unhandled promise: ${promise} with error: ${reason}`
+  )
 
-  await server.init()
+  throw reason
+})
 
-  const msg = `Server ON Listening on port: ${PORT}`
+process.on('uncaughtException', (error) => {
+  Logger.error(`App exiting due uncaught handled error: ${error}`)
 
-  server.start(msg)
+  process.exit(ExitCode.Failure)
+})
+
+enum ExitCode {
+  Failure = 1,
+  Success = 0
+}
+
+(async () => {
+  try {
+    const PORT = config.get('App.port') as number
+    const msg = `Server ON Listening on port: ${PORT}`
+    const server = new SetupServer(PORT)
+
+    await server.init()
+    server.start(msg)
+
+    const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT']
+
+    exitSignals.map((signal) => {
+      process.on(signal, async () => {
+        try {
+          await server.stop()
+
+          Logger.info('Server stopped with success')
+          process.exit(ExitCode.Success)
+        } catch (err) {
+          Logger.error(`Server stopper with failure. Error: ${err}`)
+          process.exit(ExitCode.Failure)
+        }
+      })
+    })
+  } catch (err) {
+    Logger.error(`Server exit with error: ${err}`)
+    process.exit(ExitCode.Failure)
+  }
 })()
