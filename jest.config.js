b7722e7d189923f1a2b28df34c03551a49129eef
IglanCardeal <cmtcardeal@outlook.com>
2021-09-20 09:09:01 -0300

    ci: update github actions test script to CI and update alias on package json

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /**
  * Arquivo para testes unitários que estarão junto dos arquivos a serem
  * testados.
- * 
+ *
  * Arquivos de testes de integracão ficarão dentro da pasta "test".
  */
 
@@ -10,14 +10,15 @@ const root = resolve(__dirname)
 
 module.exports = {
   rootDir: root,
-  modulePathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/dist/"],
+  modulePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
   displayName: 'unit-tests',
   testMatch: ['<rootDir>/src/**/*.test.ts'],
   testEnvironment: 'node',
+  coverageProvider: 'babel',
   clearMocks: true,
   preset: 'ts-jest',
   moduleNameMapper: {
     '@src/(.*)': '<rootDir>/src/$1',
-    '@test/(.*)': '<rootDir>/test/$1',
-  },
+    '@test/(.*)': '<rootDir>/test/$1'
+  }
 }
