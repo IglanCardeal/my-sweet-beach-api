2096aecf7cf11249c81e43937c8752b9bf988a75
IglanCardeal <cmtcardeal@outlook.com>
2021-09-20 11:39:20 -0300

    chore: add procfile and dotenv to setup mongo production connection

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -1,11 +1,18 @@
 import config, { IConfig } from 'config'
 import mongoose, { Mongoose } from 'mongoose'
+import { config as dotenv } from 'dotenv'
+
+dotenv()
 
 const dbConfig: IConfig = config.get('App.database')
 
+const env = dbConfig.util.getEnv('NODE_ENV')
+const mongoUrl =
+  env === 'production' ? process.env.MONGODB_URL_PROD : dbConfig.get('mongoUrl')
+
 export class Database {
   public static async connect (): Promise<Mongoose> {
-    return await mongoose.connect(dbConfig.get('mongoUrl'), {
+    return await mongoose.connect(mongoUrl as string, {
       useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true
