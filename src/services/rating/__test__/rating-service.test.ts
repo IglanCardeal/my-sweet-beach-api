f04643635e7c4e5a01885850e1eab5083321e79b
IglanCardeal <cmtcardeal@outlook.com>
2021-09-21 21:52:12 -0300

    test: ensure Rating Service returns 3 when crosswinds

--- src/services/rating/__test__/rating-service.test.ts
+++ src/services/rating/__test__/rating-service.test.ts
@@ -11,11 +11,11 @@ describe('Rating Service', () => {
   }
   const defaultConfig = new RatingService(beach)
 
-  // describe('Calculete rating for a given point', () => {
-  //   it.skip('', () => {
-  //     expect(1).toBeTruthy()
-  //   })
-  // })
+  describe('Calculete rating for a given point', () => {
+    it.skip('', () => {
+      expect(1).toBeTruthy()
+    })
+  })
 
   describe('Get rating bases on wind and wave positions', () => {
     it('should get rating 1 for a beach with onshore winds', () => {
@@ -24,6 +24,14 @@ describe('Rating Service', () => {
         BeachPosition.E
       )
       expect(rating).toBe(1)
+    })
+
+    it('should get rating 3 for a beach with cross winds', () => {
+      const rating = defaultConfig.getRatingBasedOnWindAndWavePositions(
+        BeachPosition.E,
+        BeachPosition.S
+      )
+      expect(rating).toBe(3)
     })
   })
 })
