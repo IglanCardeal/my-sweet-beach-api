27042cb6cee8a291e9e497e6a4e91291d03ecb90
IglanCardeal <cmtcardeal@outlook.com>
2021-09-21 21:51:39 -0300

    test: ensure Rating Service returns 1 when winds onshore

---
+++ src/services/rating/__test__/rating-service.test.ts
@@ -0,0 +1,29 @@
+import { BeachPosition } from '@src/infra/models/beach-model'
+import { RatingService } from './rating-service'
+
+describe('Rating Service', () => {
+  const beach = {
+    lat: -33.792726,
+    lng: 151.289824,
+    name: 'Manly',
+    position: 'E',
+    user: 'any user'
+  }
+  const defaultConfig = new RatingService(beach)
+
+  // describe('Calculete rating for a given point', () => {
+  //   it.skip('', () => {
+  //     expect(1).toBeTruthy()
+  //   })
+  // })
+
+  describe('Get rating bases on wind and wave positions', () => {
+    it('should get rating 1 for a beach with onshore winds', () => {
+      const rating = defaultConfig.getRatingBasedOnWindAndWavePositions(
+        BeachPosition.E,
+        BeachPosition.E
+      )
+      expect(rating).toBe(1)
+    })
+  })
+})
