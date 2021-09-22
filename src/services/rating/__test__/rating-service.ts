5fcb2b471ea1b8493c403d9d643961e80b58e745
IglanCardeal <cmtcardeal@outlook.com>
2021-09-21 21:50:49 -0300

    feat: ensure Rating Service calculates rating according wave wind and beach position

---
+++ src/services/rating/__test__/rating-service.ts
@@ -0,0 +1,37 @@
+/* eslint-disable security/detect-object-injection */
+import { BeachPosition } from '@src/infra/models/beach-model'
+import { BeachDTO } from '@src/services/beach/beach-dto'
+
+export class RatingService {
+  constructor (private readonly beach: BeachDTO) {}
+
+  public getRatingBasedOnWindAndWavePositions (
+    wavePosition: string,
+    windPosition: string
+  ): number {
+    if (wavePosition === windPosition) {
+      return 1
+    }
+    if (this.isWindOffshore(windPosition, wavePosition)) {
+      return 5
+    }
+    return 3
+  }
+
+  private isWindOffshore (windPosition: string, wavePosition: string): boolean {
+    return (
+      (wavePosition === BeachPosition.N &&
+        windPosition === BeachPosition.S &&
+        this.beach.position === BeachPosition.N) ||
+      (wavePosition === BeachPosition.S &&
+        windPosition === BeachPosition.N &&
+        this.beach.position === BeachPosition.S) ||
+      (wavePosition === BeachPosition.E &&
+        windPosition === BeachPosition.W &&
+        this.beach.position === BeachPosition.E) ||
+      (wavePosition === BeachPosition.W &&
+        windPosition === BeachPosition.W &&
+        this.beach.position === BeachPosition.W)
+    )
+  }
+}
