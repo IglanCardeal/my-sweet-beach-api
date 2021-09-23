186bbc96617be9492df45f29517a0bf5b936d6b0
IglanCardeal <cmtcardeal@outlook.com>
2021-09-23 11:45:54 -0300

    feat: ensure Rating Service returns beaches ordered ny rating

--- src/services/forecast/forecast-service.ts
+++ src/services/forecast/forecast-service.ts
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+import _ from 'lodash'
 import {
   StormGlassHttpClient,
   StormGlassForecastAPIResponseNormalized
@@ -26,43 +26,72 @@ export class ProcessForecastForBeachesService {
    * retorna os dados normalizados com a previsão do tempo para cada Beach.
    */
   public async execute (beaches: BeachDTO[]): Promise<TimeForecast[]> {
-    const pointsWithCorrectedSources: BeachForecast[] = []
-
     try {
-      for (const beach of beaches) {
-        const rating = new this.Rating(beach)
-        const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng)
-        const enrichedBeachData = this.mergeBeachAndPointsData(
-          points,
-          beach,
-          rating
-        )
-
-        pointsWithCorrectedSources.push(...enrichedBeachData)
-      }
-
-      return this.mapForecastByTime(pointsWithCorrectedSources)
+      const pointsWithCorrectedSources = await this.calculateRatings(beaches)
+      const timeForecast = this.mapForecastByTime(pointsWithCorrectedSources)
+      return timeForecast.map(forecast => ({
+        time: forecast.time,
+        forecast: _.orderBy(forecast.forecast, ['rating'], ['desc'])
+      }))
     } catch (err) {
       const error = err as any
-
       throw new ForecastProcessingInternalError(error.message)
     }
   }
 
+  private async calculateRatings (
+    beaches: BeachDTO[]
+  ): Promise<BeachForecast[]> {
+    const pointsWithCorrectedSources: BeachForecast[] = []
+    const result = await this.calculateInParallel(beaches)
+    if (result && result.length) {
+      result.forEach((res: BeachForecast[]) => {
+        pointsWithCorrectedSources.push(...res)
+      })
+    }
+    return pointsWithCorrectedSources
+  }
+
+  /**
+   * Para melhor performance, trabalhando em paralelo o cálculo de cada `Beach`.
+   */
+  private async calculateInParallel (
+    beaches: BeachDTO[]
+  ): Promise<BeachForecast[][]> {
+    const promises: Promise<BeachForecast[]>[] = []
+    for (const beach of beaches) {
+      promises.push(this.fetchAndEnrichBeachData(beach))
+    }
+    const result = await Promise.all(promises)
+    return result
+  }
+
+  private async fetchAndEnrichBeachData (
+    beach: BeachDTO
+  ): Promise<BeachForecast[]> {
+    const rating = new this.Rating(beach)
+    const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng)
+    const enrichedBeachData = this.mergeBeachAndPointsData(
+      points,
+      beach,
+      rating
+    )
+    return enrichedBeachData
+  }
+
   private mapForecastByTime (forecast: BeachForecast[]): TimeForecast[] {
     const forecastByTime: TimeForecast[] = []
-
     for (const point of forecast) {
       const timePoint = forecastByTime.find(f => f.time === point.time)
-
-      if (timePoint) timePoint.forecast.push(point)
-      else
+      if (timePoint) {
+        timePoint.forecast.push(point)
+      } else {
         forecastByTime.push({
           time: point.time,
           forecast: [point]
         })
+      }
     }
-
     return forecastByTime
   }
 
