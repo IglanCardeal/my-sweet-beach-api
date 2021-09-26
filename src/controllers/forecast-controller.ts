6606bc69abdd8a8c59395aaf8ebc5eafe44f5b35
IglanCardeal <cmtcardeal@outlook.com>
2021-09-26 00:02:15 -0300

    feat: add rate limit to forecast route

--- src/controllers/forecast-controller.ts
+++ src/controllers/forecast-controller.ts
@@ -1,5 +1,6 @@
-import { ClassMiddleware, Controller, Get } from '@overnightjs/core'
+import { ClassMiddleware, Controller, Get, Middleware } from '@overnightjs/core'
 import { Request, Response } from 'express'
+import rateLimit from 'express-rate-limit'
 
 import { authMiddleware } from '@src/infra/middlewares/auth-middle'
 import { ProcessForecastForBeachesService } from '@src/services/forecast/forecast-service'
@@ -7,13 +8,31 @@ import { GetUserBeachesService } from '@src/services/beach/get-user-beaches-serv
 import { BeachMongoRepository } from '@src/repositories/beach-repo'
 import { BaseController } from './base'
 import { Logger } from '@src/infra/logger'
+import { ApiError } from '@src/infra/utils/errors/api-error'
 
-// define um base match da rota: /forecast
+const ONE_MINUTE = 1 * 60 * 1_000
+
+const rateLimiter = rateLimit({
+  windowMs: ONE_MINUTE,
+  max: 10,
+  keyGenerator (req: Request) {
+    return req.ip
+  },
+  handler (_, res: Response) {
+    return res.status(429).send(
+      ApiError.format({
+        code: 429,
+        message: 'Too many request to the "/forecast" endpoint'
+      })
+    )
+  }
+})
+
 @Controller('forecast')
 @ClassMiddleware(authMiddleware)
-export class ForecastController extends BaseController{
-  // match em uma rota do tipo: /forecast
+export class ForecastController extends BaseController {
   @Get('')
+  @Middleware(rateLimiter)
   public async getForecastForLoggedUser (
     req: Request,
     res: Response
