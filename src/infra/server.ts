045b6c0257ee4bb126b27bf25d7c2fd519ceeee0
IglanCardeal <cmtcardeal@outlook.com>
2021-09-27 23:45:37 -0300

    refactor: make signals listeners functions more readable with forof loop

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -10,13 +10,11 @@ process.on('unhandledRejection', (reason, promise) => {
   Logger.error(
     `App exiting due unhandled promise: ${promise} with error: ${reason}`
   )
-
   throw reason
 })
 
 process.on('uncaughtException', error => {
   Logger.error(`App exiting due uncaught handled error: ${error}`)
-
   process.exit(ExitCode.Failure)
 })
 
@@ -31,17 +29,13 @@ enum ExitCode {
       (process.env.PORT as string) || (config.get('App.port') as number)
     const msg = `Server ON Listening on port: ${PORT}`
     const server = new SetupServer(+PORT)
-
     await server.init()
     server.start(msg)
-
     const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT']
-
-    exitSignals.map(signal => {
+    for (const signal of exitSignals) {
       process.on(signal, async () => {
         try {
           await server.stop()
-
           Logger.info('Server stopped with success')
           process.exit(ExitCode.Success)
         } catch (err) {
@@ -49,7 +43,7 @@ enum ExitCode {
           process.exit(ExitCode.Failure)
         }
       })
-    })
+    }
   } catch (err) {
     Logger.error(`Server exit with error: ${err}`)
     process.exit(ExitCode.Failure)
