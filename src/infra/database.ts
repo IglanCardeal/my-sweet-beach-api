3c2ef0b24f1bb2e65bf778ba911e4c249a1c15c9
IglanCardeal <cmtcardeal@outlook.com>
2021-09-29 11:37:33 -0300

    feat: verify db connection before execute db operations for beaches repository

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -1,6 +1,7 @@
 import config, { IConfig } from 'config'
 import mongoose, { Mongoose } from 'mongoose'
 import { config as dotenv } from 'dotenv'
+import { Logger } from './logger'
 
 dotenv()
 
@@ -9,14 +10,33 @@ const dbConfig: IConfig = config.get('App.database')
 const env = dbConfig.util.getEnv('NODE_ENV')
 const mongoUrl =
   env === 'production' ? process.env.MONGODB_URL_PROD : dbConfig.get('mongoUrl')
+const timeoutInMs = 30_000
 
 export class Database {
   public static async connect (): Promise<Mongoose> {
-    return await mongoose.connect(mongoUrl as string, {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+    try {
+      return await mongoose.connect(mongoUrl as string, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        connectTimeoutMS: timeoutInMs
+      })
+    } catch (err) {
+      Logger.error(`[DATABASE]: ${err}`)
+      throw err
+    }
+  }
+
+  public static getConnectionState (): string {
+    // 0 = disconnected
+    // 1 = connected
+    // 2 = connecting
+    // 3 = disconnecting
+    const state = mongoose.connection.readyState
+    if (state === 0) {
+      return 'disconnected'
+    }
+    return ''
   }
 
   public static async disconnect (): Promise<void> {
--- src/repositories/beach-repo.ts
+++ src/repositories/beach-repo.ts
@@ -1,14 +1,31 @@
+import { Database } from '@src/infra/database'
 import { BeachModel } from '@src/infra/models/beach-model'
 import { BeachDTO } from '@src/services/beach/beach-dto'
 import { BeachRepository } from '@src/services/beach/ports/beach-repo'
 
 export class BeachMongoRepository implements BeachRepository {
   async create (data: BeachDTO): Promise<void> {
+    const connectionState = Database.getConnectionState()
+    if (connectionState === 'disconnected') {
+      try {
+        await Database.connect()
+      } catch (error) {
+        throw new Error('Database Connection Error')
+      }
+    }
     const newBeach = new BeachModel(data)
     await newBeach.save()
   }
 
   async findBeachesByUserId (data: string): Promise<BeachDTO[]> {
+    const connectionState = Database.getConnectionState()
+    if (connectionState === 'disconnected') {
+      try {
+        await Database.connect()
+      } catch (error) {
+        throw new Error('Database Connection Error')
+      }
+    }
     const beaches: BeachDTO[] = await BeachModel.find({ user: data })
     return beaches
   }
